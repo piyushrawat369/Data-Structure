a51eb8528559fca276432b12aeedad2a6d9621a1
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2024-04-02 01:44:06 +0530

    Create Sliding Window.cpp

---
+++ Algorithms/Sliding Window.cpp
@@ -0,0 +1,11 @@
+int func(vector<int> &nums, int x){
+  int right = x-1, left = 0;
+  while(right < nums.size()-1){
+      sum = sum - arr[l];
+      l++;
+      r++;
+      sum = sum + arr[r];
+      ans = max(ans, sum);
+  }
+  return ans;
+}
